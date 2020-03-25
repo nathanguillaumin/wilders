ed03bf8abda9e7024ea06b0a685a0b79b4fb404f
Nathan Guillaumin <nathan.guillaumin@outlook.com>
2020-03-25 16:27:45 +0100

    initialisation npm et intégration module chalk

---
+++ index.js
@@ -0,0 +1,6 @@
+const chalk = require('chalk');
+ 
+console.log(chalk.blue('Florent'));
+console.log(chalk.black('Camille'));
+console.log(chalk.green('Anais'));
+console.log(chalk.yellow('Pierre'));
